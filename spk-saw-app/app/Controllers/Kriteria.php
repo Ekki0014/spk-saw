a50f92d1c34862e38d76cae14717a0c92013dc33
nunesaghniza <nunesaghniza11@gmail.com>
2022-11-27 00:54:19 +0700

    navbar update

---
+++ spk-saw-app/app/Controllers/Kriteria.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+class Kriteria extends BaseController
+{
+    public function index()
+    {
+        return view('kriteria/index');
+    }
+}
+?>
---
+++ spk-saw-app/app/Controllers/Periode.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+class Periode extends BaseController
+{
+    public function index()
+    {
+        return view('periode/index');
+    }
+}
+?>
